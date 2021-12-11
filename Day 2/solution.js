bc7eb70616b75b6e04fb4d1bb85dcc6fced9a3e2
zachery <z.barcoe@gmail.com>
2021-12-11 23:11:49 +0000

    day 2 solution

--- Day 2/solution.js
+++ Day 2/solution.js
@@ -32,9 +32,40 @@ const parse = (input) => {
 }
 
 
+const parse2 = (input) => {
+    let aim = 0;
+    let dist = 0;
+    let depth = 0;
+
+    for (let i = 0; i < input.length; i++) {
+        switch (input[i][0]) {
+            case 'forward':
+                dist += parseInt(input[i][1]);
+                depth += aim * parseInt(input[i][1]);
+                break;
+            case 'up':
+                //depth -= parseInt(input[i][1]);
+                aim -= parseInt(input[i][1]);
+                break;
+            case 'down':
+                //depth += parseInt(input[i][1]);
+                aim += parseInt(input[i][1]);
+                break;
+            default:
+                break;
+        }
+
+        //console.log(dist, depth, aim);
+    }
+
+    return (depth*dist)
+}
+
+
 const main = () => {
     const input = getInput();
     console.log(parse(input));
+    console.log(parse2(input));
 }
 
 main();
